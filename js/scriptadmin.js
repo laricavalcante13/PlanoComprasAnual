38632e24d2a7f631826dce2a9b4a54caeaa7c58d
itintern8 <assistenteti@crn8.org.br>
2025-12-19 09:41:59 -0300

    Update scriptadmin.js

--- js/scriptadmin.js
+++ js/scriptadmin.js
@@ -22,7 +22,9 @@ let setores = [
     { id: 20, nome: 'Cadastro', descricao: '', status: 'ativo' }
 ];
 
-let usuarios = [];
+let usuarios = [
+    { id: 1, nome: 'Admin', email: "ti@crn8.org.br", setores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20] }
+];
 
 let permissoes = {};
 usuarios.forEach(u => {
